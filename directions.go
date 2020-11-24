0f938342eb3a663893892118bd4f93ee764cf9dd
MiguelGutierrez <imickey2010@hotmail.com>
2020-11-24 12:24:14 -0600

    Files created!

---
+++ directions.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func dirReduc(arr []string) []string {
+	
+}
+
+func main() {
+	lista := []string{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}
+	fmt.Println(dirReduc(lista))
+}
